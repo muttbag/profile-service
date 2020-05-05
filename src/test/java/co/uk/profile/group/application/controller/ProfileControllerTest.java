d07407879afb2e08b4ee4d539f62343d4a2e63f8
Martin <mrmartyn05@hotmail.co.uk>
2020-05-05 12:38:54 +0100

    Added unit test for jenkins

---
+++ src/test/java/co/uk/profile/group/application/controller/ProfileControllerTest.java
@@ -0,0 +1,13 @@
+package co.uk.profile.group.application.controller;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProfileControllerTest {
+
+    @Test
+    public void test1(){
+        Assertions.assertEquals("test", "test");
+    }
+}
