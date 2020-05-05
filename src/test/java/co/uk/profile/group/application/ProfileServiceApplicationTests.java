f15876b65e18cc1102ac705e07287ca35602c9b2
Martin <mrmartyn05@hotmail.co.uk>
2020-05-05 11:30:59 +0100

    Corrected unit test class naming

---
+++ src/test/java/co/uk/profile/group/application/ProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uk.profile.group.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
